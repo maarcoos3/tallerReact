94ce5842fb04ee25d1c27691f71454eee4d3f5a7
maarcoos3 <118178396+maarcoos3@users.noreply.github.com>
2024-11-14 19:47:39 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+import Descripcion from './componentes/descripcion'; 
+import EscaleraFotos from './componentes/escaleraFotos';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+      </header>
+      <body>
+      <Descripcion />
+      <EscaleraFotos />
+      </body>
+    </div>
+  );
+}
+
+export default App;
